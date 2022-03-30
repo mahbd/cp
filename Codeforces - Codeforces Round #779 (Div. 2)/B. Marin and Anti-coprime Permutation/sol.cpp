f8694fdccc377179bc99c71ce577d38844787a23
Mahmudul Alam <mahmudula2000@gmail.com>
2022-03-30 17:30:43 +0600

    Solved 779 B

--- Codeforces - Codeforces Round #779 (Div. 2)/B. Marin and Anti-coprime Permutation/sol.cpp
+++ Codeforces - Codeforces Round #779 (Div. 2)/B. Marin and Anti-coprime Permutation/sol.cpp
@@ -10,8 +10,29 @@ using namespace std;
     cin >> test_case; \
     for (ll tcv = 0; tcv < test_case; tcv++)
 
+const ll mod = 998244353;
+
+ll fact(ll n) {
+    ll ans = 1;
+    for (ll i = 1; i <= n; i++) {
+        ans = (ans * i) % mod;
+    }
+    for (ll i = 1; i <= n; i++) {
+        ans = (ans * i) % mod;
+    }
+    return ans;
+}
+
 int main() {
     fast_input;
-
+    tc() {
+        ll n;
+        cin >> n;
+        if (n & 1) {
+            cout << 0 << endl;
+            continue;
+        }
+        cout << fact(n / 2) << endl;
+    }
     return 0;
 }
