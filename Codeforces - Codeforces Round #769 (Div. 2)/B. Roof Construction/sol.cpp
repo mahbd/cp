003f6ef50fe8fd2cc9e0dac5ed2e92f69b70388a
Mahmudul Alam <mahmudula2000@gmail.com>
2022-04-07 15:30:18 +0600

    Solved 1632 B

--- Codeforces - Codeforces Round #769 (Div. 2)/B. Roof Construction/sol.cpp
+++ Codeforces - Codeforces Round #769 (Div. 2)/B. Roof Construction/sol.cpp
@@ -1,31 +1,33 @@
-#include<bits/stdc++.h>
- 
+#include <bits/stdc++.h>
+
 using namespace std;
 #define ll long long
-#define fast_input ios_base::sync_with_stdio(false); cin.tie(0)
-#define tc() ll test_case; cin >> test_case; for(ll tcv = 0; tcv < test_case; tcv++)
- 
+#define fast_input                    \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(0)
+#define tc()          \
+    ll test_case;     \
+    cin >> test_case; \
+    for (ll tcv = 0; tcv < test_case; tcv++)
+
 int main() {
     fast_input;
     tc() {
         ll n;
         cin >> n;
-        ll lowest = n;
-        for (int i = 0; i < 32; i++) {
-            ll num = n;
-            num >>= i;
-            num <<= i;
-            if (num != 0) {
-                lowest = min(lowest, num);
-            }
+        ll k = 1;
+        while (2 * k < n) {
+            k *= 2;
         }
-        for (ll i = 1; i <= n; i++) {
-            if (i == lowest) {
-                cout << 0 << " ";
-            }
+
+        for (int i = k - 1; i >= 0; i--) {
+            cout << i << " ";
+        }
+        for (int i = k; i < n; i++) {
             cout << i << " ";
         }
         cout << endl;
+
     }
     return 0;
 }
