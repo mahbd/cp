20a953b9190fe584bb68539a35154a42df0b617a
Mahmudul Alam <mahmudula2000@gmail.com>
2023-10-15 23:26:35 +0600

    solved problem A

--- acmx/Codeforces - Codeforces Round 849 (Div. 4)/A. Codeforces Checking/sol.cpp
+++ acmx/Codeforces - Codeforces Round 849 (Div. 4)/A. Codeforces Checking/sol.cpp
@@ -1,12 +1,23 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 
 using namespace std;
 typedef long long ll;
 #define endl '\n'
 
-
 int main() {
     ios::sync_with_stdio(false), cin.tie(nullptr);
-    
+    set<char> codeforces = {'c', 'o', 'd', 'e', 'f', 'o', 'r', 'c', 'e', 's'};
+    ll test_case;
+    cin >> test_case;
+    for (int tcv = 0; tcv < test_case; tcv++) {
+        char s;
+        cin >> s;
+        if (codeforces.count(s)) {
+            cout << "YES" << endl;
+        } else {
+            cout << "NO" << endl;
+        }
+    }
+
     return 0;
 }
