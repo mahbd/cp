9cc4d051df6e1787f9f5d0bf120b157757626cce
Mahmudul Alam <mahmudula2000@gmail.com>
2023-10-16 08:10:59 +0600

    Solved C of div 4

---
+++ acmx/Codeforces - Codeforces Round 849 (Div. 4)/C. Prepend and Append/readme.md
@@ -0,0 +1,9 @@
+# Simulate the problem in reverse style
+
+# Two Pointer
+
+1. Start from start and end
+2. check if they are are.
+3. If equal break
+4. Otherwise reduce size
+5. print final size
--- acmx/Codeforces - Codeforces Round 849 (Div. 4)/C. Prepend and Append/sol.cpp
+++ acmx/Codeforces - Codeforces Round 849 (Div. 4)/C. Prepend and Append/sol.cpp
@@ -1,12 +1,27 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 
 using namespace std;
 typedef long long ll;
 #define endl '\n'
 
-
 int main() {
     ios::sync_with_stdio(false), cin.tie(nullptr);
-    
+    ll test_case;
+    cin >> test_case;
+    for (int tcv = 0; tcv < test_case; tcv++) {
+        ll n;
+        string s;
+        cin >> n >> s;
+        ll start = 0, end = s.size() - 1;
+        while (start < end) {
+            if (s[start] != s[end]) {
+                start++;
+                end--;
+            } else
+                break;
+        }
+        cout << end - start + 1 << endl;
+    }
+
     return 0;
 }
