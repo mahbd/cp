9ae94a7236525ab236978213c5ce68278c4d756b
Mahmudul Alam <mahmudula2000@gmail.com>
2023-10-16 08:42:25 +0600

    Solved D of div4

---
+++ acmx/Codeforces - Codeforces Round 849 (Div. 4)/D. Distinct Split/readme.md
@@ -0,0 +1,6 @@
+# Iterate from start to end
+
+1. Create two map m1 and m2.
+2. Add all character to m1.
+3. Now add character to m1 and remove that from m2, then check size.
+4. Remember to erase element from m2 if count goes to 0.
--- acmx/Codeforces - Codeforces Round 849 (Div. 4)/D. Distinct Split/sol.cpp
+++ acmx/Codeforces - Codeforces Round 849 (Div. 4)/D. Distinct Split/sol.cpp
@@ -1,12 +1,32 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 
 using namespace std;
 typedef long long ll;
 #define endl '\n'
 
-
 int main() {
     ios::sync_with_stdio(false), cin.tie(nullptr);
-    
+    ll test_case;
+    cin >> test_case;
+    for (int tcv = 0; tcv < test_case; tcv++) {
+        ll n;
+        string s;
+        cin >> n >> s;
+        map<char, int> m1, m2;
+        for (auto c : s) {
+            m2[c]++;
+        }
+        ll ans = 0;
+        for (int i = 0; i < n; i++) {
+            m1[s[i]]++;
+            m2[s[i]]--;
+            if (m2[s[i]] == 0) {
+                m2.erase(s[i]);
+            }
+            ans = max(ans, ll(m1.size() + m2.size()));
+        }
+        cout << ans << endl;
+    }
+
     return 0;
 }
