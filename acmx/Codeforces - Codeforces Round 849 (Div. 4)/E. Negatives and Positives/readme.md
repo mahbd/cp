59b708e72d53564cd2942a00d6418b8ff08c09e9
Mahmudul Alam <mahmudula2000@gmail.com>
2023-10-17 09:23:26 +0600

    solved problem E of div4

---
+++ acmx/Codeforces - Codeforces Round 849 (Div. 4)/E. Negatives and Positives/readme.md
@@ -0,0 +1,4 @@
+# Single Minimum element will be reduced
+1. As you have to change sign of two element, you can move the sign any place of the array.
+2. If number of negative element is even then, we can make all number positive.
+3. If there are odd number of negative element, then one number must be negative. We can make the minimum number negative.
--- acmx/Codeforces - Codeforces Round 849 (Div. 4)/E. Negatives and Positives/sol.cpp
+++ acmx/Codeforces - Codeforces Round 849 (Div. 4)/E. Negatives and Positives/sol.cpp
@@ -1,12 +1,32 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 
 using namespace std;
 typedef long long ll;
 #define endl '\n'
 
-
 int main() {
     ios::sync_with_stdio(false), cin.tie(nullptr);
-    
+    ll test_case;
+    cin >> test_case;
+    for (int tcv = 0; tcv < test_case; tcv++) {
+        ll n;
+        cin >> n;
+        vector<ll> arr(n);
+        ll sum = 0, neg = 0;
+        for (auto &i : arr) {
+            cin >> i;
+            if (i < 0) {
+                i *= -1;
+                sum += i;
+                neg++;
+            } else {
+                sum += i;
+            }
+        }
+        if (neg % 2) {
+            sum -= *min_element(arr.begin(), arr.end()) * 2;
+        }
+        cout << sum << endl;
+    }
     return 0;
 }
